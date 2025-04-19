018c0e67f77c333eb3a740d83f58a6220180d7e2
Projacktor <1234xl.com@gmail.com>
2025-04-19 15:42:22 +0300

    chore: add ReactQuery devtools

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -17,3 +17,46 @@ export const createMessage = async (messageData: MessageData): Promise<ViewMessa
 
   return response.json()
 }
+
+export const getLlmMessage = async (dialog_id: string) => {
+  const model = 'deepseek-r1-distill-qwen-32b'
+  const h = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      dialog_id: dialog_id,
+      model: model
+    })
+  }
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/messages/receive`, h)
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok')
+  }
+
+  return response.json()
+}
+
+export const getMessages = async (dialog_id: string, amount: number): Promise<ViewMessage[]> => {
+  const h = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      dialog_id: dialog_id,
+      amount: amount
+    })
+  }
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/messages/get`, h)
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok')
+  }
+
+  return response.json()
+}
